6d55998ed898ed9cbcdc7ba969cd4fe8e7cdea3f
Joel Elias Méndez <67445275+joelmendez1@users.noreply.github.com>
2022-05-29 20:05:55 -0300

    add medias queries (#4)

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,33 @@
 //DOM ELEMENTS
-const operacion = document.getElementById('operacion')
-const tipologia = document.getElementById('tipologia')
-const dormitorios = document.getElementById('dormitorios')
-
-const mainButtons = [operacion, tipologia, dormitorios]
+const filterButtons = document.getElementsByClassName('main__filters')[0].children
 
 //Functions
 const showElement = (element, addClass, deleteClass) => {
-    classtype = element.className
+    classtype = element.className;
 
     switch(classtype) {
         case addClass:
-            element.classList.remove(addClass)
-            element.classList.add(deleteClass)
+            element.classList.remove(addClass);
+            element.classList.add(deleteClass);
             break
         case deleteClass:
-            element.classList.remove(deleteClass)
+            element.classList.remove(deleteClass);
             element.classList.add(addClass)
-            break
+            break;
     }
-}
+};
 
-mainButtons.forEach(button => {
+[...filterButtons].forEach(button => {
     button.addEventListener('click', () => {
         const articles = button.parentNode.nextElementSibling.children;
 
         for(let i = 0; i < articles.length; i++) {
             if(articles[i].className === 'main__filters-article-active') {
                 showElement(articles[i], 'main__filters-article', 'main__filters-article-active');
-            }else if (button === mainButtons[i]) {
+            }else if (button === [...filterButtons][i]) {
                 showElement(articles[i], 'main__filters-article', 'main__filters-article-active');
             }
         }
     });
-})
+});
+
