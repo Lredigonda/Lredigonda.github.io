fa0f0885ba26c6e58b403e655217fbb2eb8b37ca
Joel Elias Méndez <67445275+joelmendez1@users.noreply.github.com>
2022-05-29 18:52:32 -0300

    improve script and styles (#3)

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,36 @@
 //DOM ELEMENTS
 const operacion = document.getElementById('operacion')
-const operacionArticle = document.getElementsByClassName('operacion_article')[0]
+const tipologia = document.getElementById('tipologia')
+const dormitorios = document.getElementById('dormitorios')
+
+const mainButtons = [operacion, tipologia, dormitorios]
 
 //Functions
-
-const showElement = (element) => {
+const showElement = (element, addClass, deleteClass) => {
     classtype = element.className
 
     switch(classtype) {
-        case 'operacion_article':
-            element.classList.remove('operacion_article')
-            element.classList.add('operacion_article-show')
+        case addClass:
+            element.classList.remove(addClass)
+            element.classList.add(deleteClass)
             break
-        case 'operacion_article-show':
-            element.classList.remove('operacion_article-show')
-            element.classList.add('operacion_article')
+        case deleteClass:
+            element.classList.remove(deleteClass)
+            element.classList.add(addClass)
             break
     }
 }
 
-operacion.addEventListener('click', () => {
-    showElement(operacionArticle)
+mainButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const articles = button.parentNode.nextElementSibling.children;
+
+        for(let i = 0; i < articles.length; i++) {
+            if(articles[i].className === 'main__filters-article-active') {
+                showElement(articles[i], 'main__filters-article', 'main__filters-article-active');
+            }else if (button === mainButtons[i]) {
+                showElement(articles[i], 'main__filters-article', 'main__filters-article-active');
+            }
+        }
+    });
 })
